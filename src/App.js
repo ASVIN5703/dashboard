be0604c7803bac9f34346f50d06403cdac704ef4
ASVIN <asvin.vt2021csea@sece.ac.in>
2023-06-30 12:55:05 +0530

    First time pull

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { Routes,Route } from 'react-router-dom';
 import './App.css';
+import MainDash from './components/MainDash/MainDash';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Profile from './components/Profile/Profie';
+
+// import Complaints from './components/Complaints/Complaints';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+         <div className='appGlass'>
+         <Sidebar/>
+         <Routes>
+           
+             
+              <Route path="/profile" element={<Profile/>}/>
+              
+          
+            
+         </Routes>
+         </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import './Sidebar.css'
+import { useState } from 'react';
+
+import { SidebarData } from '../../Data/Data';
+import {UilSignOutAlt} from '@iconscout/react-unicons';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const Sidebar = () => {
+      const navigate=useNavigate();
+      const[selected,setSelected] = useState(0);
+      const handleSidebarItemClick = (index) => {
+        setSelected(index);
+        navigate(SidebarData[index].path); // Assuming you have a 'path' property in each SidebarData item
+      };
+     
+
+  return (
+    <div className='Sidebar'>
+        {/* logo */}
+          <div className='logo'>
+            <img src='https://tinyurl.com/mrfix-logo' alt='img-error'/>
+            <span>
+                Mr.Fix
+            </span>
+          </div>
+
+          {/* menu */}
+          <div className='menu'>
+              {SidebarData.map((item,index)=>{
+                return(
+                  <div className={selected===index?'menuItem active':'menuItem'}
+                  key={index}
+                  onClick={() => handleSidebarItemClick(index)}>
+                        <item.icon/>
+                        <span>
+                          {item.heading}
+                        </span>
+                  </div>
+                )
+              })}
+              <div className='menuItem'>
+                <UilSignOutAlt/>LogOut
+              </div>
+          </div>
+    </div>
+  )
+}
+
+export default Sidebar;
